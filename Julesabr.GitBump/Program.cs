5af4dc9dd7ea546093e6c8b60f455a1d5430c76c
Julian Sangillo <juliansangillo@gmail.com>
2022-12-24 15:42:49 -0500

    Add implementation of Main

--- Julesabr.GitBump/Program.cs
+++ Julesabr.GitBump/Program.cs
@@ -1,6 +1,17 @@
-﻿namespace Julesabr.GitBump {
+﻿using CommandLine;
+using JetBrains.Annotations;
+using Julesabr.IO;
+using Julesabr.LibGit;
+
+namespace Julesabr.GitBump {
+    [UsedImplicitly]
     internal static class Program {
-        public static void Main(string[] args) {
+        [UsedImplicitly]
+        public static int Main(string[] args) {
+            Controller controller = new(IRepository.Create(), new FileFactory());
+            Options options = Parser.Default.ParseArguments<Options>(args).Value;
+            
+            return (int) controller.GitBump(options);
         }
     }
 }
